4fb566b84a327ac0128c36ce66bb4a768c6de06f
Hallam Fulcher <hallam.fulcher@gmail.com>
2020-12-29 17:35:34 +0000

    Artist test passing

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const artistControllers = require('./controllers/artists');
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.json('Hello World!');
+  });
+
+  app.post('/artists', artistControllers.create);
+
+module.exports = app;
